a1591f6e1253284d69ce02851a4f055e3657098d
pricilla gumato sharamo <gumatopricilla22@gmail.com>
2019-07-10 11:56:56 +0300

    creating templates files

--- shop/urls.py
+++ shop/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^shopapp/',include('shopapp.urls'))
+    url(r'^',include('shopapp.urls'))
 ]
--- shopapp/views.py
+++ shopapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
+import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to Easyshop')
+    return render(request,'welcome.html')
