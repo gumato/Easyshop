5815bcf99b5ced2e2e797cf8e215dba93a1eb589
pricilla gumato sharamo <gumatopricilla22@gmail.com>
2019-07-10 12:30:18 +0300

    installing bootstrap

--- shopapp/views.py
+++ shopapp/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
 import datetime as dt
 
 # Create your views here.
-def welcome(request):
-    return render(request,'welcome.html')
+def index(request):
+    date = dt.date.today()
+    return render(request,'index.html')
