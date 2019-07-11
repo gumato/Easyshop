69e991a4cced4cc19812ec2707dd3c169be089db
pricilla gumato sharamo <gumatopricilla22@gmail.com>
2019-07-11 11:39:50 +0300

    add search file

--- shopapp/views.py
+++ shopapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
 import datetime as dt
+from .models import Category,Product
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
